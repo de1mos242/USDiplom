0726225d3c57d59f06949f1ae4c984b755872688
de1mos <de1m0s242@gmail.com>
2012-06-09 00:42:20 +0800

    thin

--- line3d.cpp
+++ line3d.cpp
@@ -11,7 +11,7 @@ void Line3D::setPoints(Point3D from, Point3D to) {
 
 void Line3D::draw() {
     Figure3D::draw();
-    glLineWidth(3.0f);
+    glLineWidth(1.0f);
     glBegin(GL_LINES);
         glVertex3f(from.x,from.y,from.z);
         glVertex3f(to.x,to.y,to.z);
